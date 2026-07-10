f7a62b964d06bfdac35f3facfa4cd877b4216138
AxelAxel97 <carlphilipsaclinton@gmail.com>
2026-07-10 17:10:01 +0200

    Separate home, shop, and subscription pages

--- fitness_subscription_app/urls.py
+++ fitness_subscription_app/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from shop import views as shop_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),
+    path('', shop_views.home, name='home'),
+    path('shop/', include('shop.urls')),
     path('accounts/', include('accounts.urls')),
     path('community/', include('community.urls')),
     path('subscriptions/', include('subscriptions.urls')),
